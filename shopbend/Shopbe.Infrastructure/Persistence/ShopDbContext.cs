4b45ae63029fc024bb5e57493b999cbb9b7a3a20
loihnt05 <23520869@gm.uit.edu.vn>
2026-03-11 19:10:51 +0700

    setup dbcontext

--- shopbend/Shopbe.Infrastructure/Persistence/ShopDbContext.cs
+++ shopbend/Shopbe.Infrastructure/Persistence/ShopDbContext.cs
@@ -1,1 +1,15 @@
-namespace 
+using Microsoft.EntityFrameworkCore;
+using Shopbe.Domain.Entities;
+
+namespace Shopbe.Infrastructure.Persistence;
+
+public class ShopDbContext : DbContext
+{
+    public ShopDbContext(DbContextOptions<ShopDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Product> Products { get; set; }
+    public DbSet<Order> Orders { get; set; }
+    public DbSet<OrderItem> OrderItems { get; set; }
+}
