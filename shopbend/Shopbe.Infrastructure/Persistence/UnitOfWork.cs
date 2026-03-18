70f293899b4a3871a5a9651f551323de86355375
loihnt05 <23520869@gm.uit.edu.vn>
2026-03-18 19:40:00 +0700

    mini fix unit of work

--- shopbend/Shopbe.Infrastructure/Persistence/UnitOfWork.cs
+++ shopbend/Shopbe.Infrastructure/Persistence/UnitOfWork.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore.Storage;
+using Shopbe.Application.Common.Interfaces;
 using Shopbe.Application.Interfaces;
+using Shopbe.Application.Interfaces.Repositories;
 using Shopbe.Infrastructure.Repositories;
 
 namespace Shopbe.Infrastructure.Persistence;
@@ -10,12 +12,16 @@ public class UnitOfWork : IUnitOfWork
     private IDbContextTransaction? _transaction;
     public IProductRepository Product { get; }
     public ICategoryRepository Category { get; }
+    public IProductVariantRepository ProductVariant { get; }
+    public IProductImageRepository ProductImage { get; }
 
     public UnitOfWork(ShopDbContext context)
     {
         _context = context;
         Product = new ProductRepository(context);
         Category = new CategoryRepository(context);
+        ProductImage = new ProductImageRepository(context);
+        ProductVariant = new ProductVariantRepository(context);
     }
     public async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
     {
