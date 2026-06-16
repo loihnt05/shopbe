90745bdcb79127185cf58f7e179244a0c7308022
loihnt05 <23520869@gm.uit.edu.vn>
2026-06-16 09:24:51 +0700

    mini fix tailwind

--- shopfend/postcss.config.mjs
+++ shopfend/postcss.config.mjs
@@ -1,13 +1,6 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-
-const __dirname = dirname(fileURLToPath(import.meta.url));
-
 const config = {
   plugins: {
-    "@tailwindcss/postcss": {
-      base: __dirname,
-    },
+    "@tailwindcss/postcss": {},
   },
 };
 
